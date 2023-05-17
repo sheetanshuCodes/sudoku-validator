2299cd6941c2baa5c43e2092a8f26a1353124266
Sheetanshu Shekhar <62210159+sheetanshuCodes@users.noreply.github.com>
2023-05-18 00:15:52 +0530

    Fixed styling issues and added some comments

--- newApp.js
+++ newApp.js
@@ -1,101 +1,38 @@
-const sudokuValidator = (arr) => {
-  // The array validationArray is used to validate whether any
-  // of the rows, columns or 3x3 grids contain a number
-  // more than once.
+function sudokuValidator(arr) {
   const validationArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  // trueFalse array store an array of results in Boolean format.
-  // These results come from validating each row, column and 3x3 matrix.
-  // In the end, we check whether any of the elements in this array is false.
-  // If it is, the whole test fails and we infer that the input array
-  // is not a valid sudoku solution. If all elements of this array are true,
-  // we infer that it's a valid sudoku solution.
-  const trueFalse = [];
-
-  //Create an array of 3x3 grids present in the sudoku solution.
-  const threeByThreeMatrix = [];
-  function updateMatrix(a, b) {
-    const threeByThreeArray = [];
-    for (i = a; i < a + 3; i++) {
-      for (j = b; j < b + 3; j++) {
-        threeByThreeArray.push(arr[i][j]);
-      }
-    }
-    threeByThreeMatrix.push(threeByThreeArray);
-  }
-  for (m = 0; m < 9; m += 3) {
-    for (n = 0; n < 9; n += 3) {
-      updateMatrix(m, n);
-    }
+  // Function to validate rows, columns, and 3x3 grids
+  function validate(array) {
+    const sortedArr = array.sort((a, b) => a - b);
+    return sortedArr.every((num, index) => num === validationArray[index]);
   }
 
-  //Validate 3x3 grids in the sudoku solution.
-  //Loop through each 3x3 grid to check whether all grids contain numbers from 1 to 9 exactly once.
-  threeByThreeMatrix.forEach((row) => {
-    const sortedArr = row.sort((a, b) => a - b);
-    const list = sortedArr.filter((num, index) => {
-      if (num !== validationArray[index]) {
-        return true;
-      }
-    });
-    //Update isValid array above with true, false.
-    if (list.length === 0) {
-      trueFalse.push(true);
-    } else {
-      trueFalse.push(false);
-    }
-  });
+  // Validate rows
+  const rowsValid = arr.every((row) => validate(row));
 
-  //Create a transposed array from the arr to check for the columns.
-  const verticalArray = [];
-  for (i = 0; i < 9; i++) {
-    const transposedArray = arr.map((row) => row[i]);
-    verticalArray.push(transposedArray);
-  }
-  //Validate columns in the sudoku solution.
-  //Loop through each column to check whether all grids contain numbers from 1 to 9 exactly once.
-  verticalArray.forEach((row) => {
-    const sortedArr = row.sort((a, b) => a - b);
-    const list = sortedArr.filter((num, index) => {
-      if (num !== validationArray[index]) {
-        return true;
+  // Validate columns
+  const colsValid = arr[0]
+    .map((col, i) => arr.map((row) => row[i]))
+    .every((col) => validate(col));
+
+  // Validate 3x3 grids
+  let gridsValid = true;
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      const grid = [].concat(
+        arr[i].slice(j, j + 3),
+        arr[i + 1].slice(j, j + 3),
+        arr[i + 2].slice(j, j + 3)
+      );
+      if (!validate(grid)) {
+        gridsValid = false;
+        break;
       }
-    });
-    if (list.length === 0) {
-      trueFalse.push(true);
-    } else {
-      trueFalse.push(false);
     }
-  });
-  //Create a array from the arr to check for the rows.
-  const horizontalArray = [];
-  for (i = 0; i < 9; i++) {
-    const horizontalSinArr = arr[i].map((num) => num);
-    horizontalArray.push(horizontalSinArr);
   }
-  //Validate rows in the sudoku solution.
-  //Loop through each row to check whether all grids contain numbers from 1 to 9 exactly once.
-  horizontalArray.forEach((row) => {
-    const sortedArr = row.sort((a, b) => a - b);
-    const list = sortedArr.filter((num, index) => {
-      if (num !== validationArray[index]) {
-        return true;
-      }
-    });
-    if (list.length === 0) {
-      trueFalse.push(true);
-    } else {
-      trueFalse.push(false);
-    }
-  });
-  // Return false if the trueFalse array contains only trues.
-  // If it contains even a single false, return false.
-  if (trueFalse.includes(false)) {
-    return false;
-  } else {
-    return true;
-  }
-};
+
+  return rowsValid && colsValid && gridsValid;
+}
 
 const sudokuForm = document.querySelector(".sudokuForm");
 const displayValid = document.querySelector(".displayValid");
